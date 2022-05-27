8cd171b11ab6eb852d8f640fc8d35389f16d7e99
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-05-27 13:32:45 +0000

    test: :white_check_mark: Test applyFilter
    Test of applyFilter custom is done

---
+++ src/@seedwork/domain/repository/__tests__/in-memory-searchable.repository.spec.ts
@@ -0,0 +1,73 @@
+import Entity from "../../entity/entity";
+import { InMemorySearchableRepository } from "../in-memory.repository";
+
+type StubEntityProps = {
+  name: string;
+  price: number;
+};
+
+class StubEntity extends Entity<StubEntityProps> {}
+
+class StubInMemorySearchableRepository extends InMemorySearchableRepository<StubEntity> {
+  sortableFields: string[] = ["name"];
+  protected async applyFilter(
+    items: StubEntity[],
+    filter: string | null
+  ): Promise<StubEntity[]> {
+    if (!filter) {
+      return items;
+    }
+
+    return items.filter((i) => {
+      return (
+        i.props.name.toLowerCase().includes(filter.toLowerCase()) ||
+        i.props.price.toString() === filter
+      );
+    });
+  }
+}
+
+describe("InMemorySearchableRepository Unit Test", () => {
+  let repository: StubInMemorySearchableRepository;
+
+  beforeEach(() => (repository = new StubInMemorySearchableRepository()));
+  describe("ApplyFilter method", () => {
+    it("should no filter items when filter param is null", async () => {
+      const items = [
+        new StubEntity({ name: "test", price: 1 }),
+        new StubEntity({ name: "test2", price: 2 }),
+      ];
+      const spyFilterMethod = jest.spyOn(items, "filter" as any);
+      const itemsFiltered = await repository["applyFilter"](items, null);
+
+      expect(itemsFiltered).toEqual(items);
+      expect(spyFilterMethod).not.toHaveBeenCalled();
+    });
+
+    it("should filter using a filter param", async () => {
+      const items = [
+        new StubEntity({ name: "test", price: 3 }),
+        new StubEntity({ name: "TEST", price: 10 }),
+        new StubEntity({ name: "fake", price: 10 }),
+      ];
+      const spyFilterMethod = jest.spyOn(items, "filter" as any);
+      let itemsFiltered = await repository["applyFilter"](items, "TEST");
+      expect(itemsFiltered).toEqual([items[0], items[1]]);
+      expect(spyFilterMethod).toHaveBeenCalledTimes(1);
+
+      itemsFiltered = await repository["applyFilter"](items, "10");
+      expect(itemsFiltered).toEqual([items[1], items[2]]);
+      expect(spyFilterMethod).toHaveBeenCalledTimes(2);
+
+      itemsFiltered = await repository["applyFilter"](items, "no-filter");
+      expect(itemsFiltered).toHaveLength(0);
+      expect(spyFilterMethod).toHaveBeenCalledTimes(3);
+    });
+  });
+
+  describe("ApplySort method", () => {});
+
+  describe("ApplyPaginate method", () => {});
+
+  describe("search method", () => {});
+});
