69ceafdf2f47d08982d92164852e882c85a06ced
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-05-27 13:58:21 +0000

    test: :white_check_mark: Test applySort
    Test applySort is Done

--- src/@seedwork/domain/repository/__tests__/in-memory-searchable.repository.spec.ts
+++ src/@seedwork/domain/repository/__tests__/in-memory-searchable.repository.spec.ts
@@ -65,7 +65,33 @@ describe("InMemorySearchableRepository Unit Test", () => {
     });
   });
 
-  describe("ApplySort method", () => {});
+  describe("ApplySort method", () => {
+    it("should not sort items", async () => {
+      const items = [
+        new StubEntity({ name: "banana", price: 1 }),
+        new StubEntity({ name: "Amora", price: 2 }),
+        new StubEntity({ name: "Abelha", price: 3 }),
+      ];
+      let itemsSorted = await repository["applySort"](items, null, null);
+      expect(itemsSorted).toStrictEqual(items);
+      itemsSorted = await repository["applySort"](items, "price", "asc");
+      expect(itemsSorted).toStrictEqual(items);
+    });
+
+    it("should sort items", async () => {
+      const items = [
+        new StubEntity({ name: "banana", price: 1 }),
+        new StubEntity({ name: "Amora", price: 2 }),
+        new StubEntity({ name: "Abelha", price: 3 }),
+      ];
+      let itemsSorted = await repository["applySort"](items, "name", "asc");
+      expect(itemsSorted).toStrictEqual([items[2], items[1], items[0]]);
+      itemsSorted = await repository["applySort"](items, "name", "desc");
+      expect(itemsSorted).toStrictEqual([items[0], items[1], items[2]]);
+      itemsSorted = await repository["applySort"](items, "price", "asc");
+      expect(itemsSorted).toStrictEqual(items);
+    });
+  });
 
   describe("ApplyPaginate method", () => {});
 
