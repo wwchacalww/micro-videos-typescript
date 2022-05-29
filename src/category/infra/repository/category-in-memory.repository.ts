e3ced3ed9f96b7b34b672847ed6caf2979b68d6a
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-05-29 12:42:17 -0300

    Refactory: Namespace in category repository

--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -1,7 +1,21 @@
 import { InMemorySearchableRepository } from "../../../@seedwork/domain/repository/in-memory.repository";
 import { Category } from "../../domain/entities/category";
-import CategoryRepositoryInterface from "../../domain/repository/category.repository";
+import CategoryRepository from "../../domain/repository/category.repository";
 
 export default class CategoryInMemoryRepository
   extends InMemorySearchableRepository<Category>
-  implements CategoryRepositoryInterface {}
+  implements CategoryRepository.Repository
+{
+  protected async applyFilter(
+    items: Category[],
+    filter: CategoryRepository.Filter
+  ): Promise<Category[]> {
+    if (!filter) {
+      return items;
+    }
+
+    return items.filter((i) => {
+      return i.props.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  }
+}
