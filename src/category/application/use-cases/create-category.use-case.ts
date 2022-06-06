b0eeb47a0e6709fa7655f9a45494a1611f1d9781
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-06-06 09:33:09 -0300

    Interface UseCase created

--- src/category/application/use-cases/create-category.use-case.ts
+++ src/category/application/use-cases/create-category.use-case.ts
@@ -1,6 +1,7 @@
 import { Category } from "../../domain/entities/category";
 import CategoryRepository from "../../domain/repository/category.repository";
 import { CategoryOutput } from "../dto/category-output.dto";
+import UseCase from "../../../@seedwork/application/use-case";
 
 export type Input = {
   name: string;
@@ -10,7 +11,7 @@ export type Input = {
 
 export type Output = CategoryOutput;
 
-export default class CreateCategoryUseCase {
+export default class CreateCategoryUseCase implements UseCase<Input, Output> {
   constructor(private categoryRepo: CategoryRepository.Repository) {}
   async execute(input: Input): Promise<Output> {
     const category = new Category(input);
