d55bf333a1da1260a1481e54bb6af9fdc6b6a81a
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-06-06 08:30:03 -0300

    GetCategoryUseCase created

--- src/category/application/use-cases/get-category.use-case.ts
+++ src/category/application/use-cases/get-category.use-case.ts
@@ -1,20 +1,16 @@
-import { Category } from "../../domain/entities/category";
 import CategoryRepository from "../../domain/repository/category.repository";
 import { CategoryOutput } from "../dto/category-output.dto";
 
 export type Input = {
-  name: string;
-  description?: string;
-  is_active?: boolean;
+  id: string;
 };
 
 export type Output = CategoryOutput;
 
-export default class CreateCategoryUseCase {
+export default class GetCategoryUseCase {
   constructor(private categoryRepo: CategoryRepository.Repository) {}
   async execute(input: Input): Promise<Output> {
-    const category = new Category(input);
-    await this.categoryRepo.insert(category);
+    const category = await this.categoryRepo.findById(input.id);
     return {
       id: category.id,
       name: category.name,
