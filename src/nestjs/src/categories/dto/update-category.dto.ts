a917666490a3a75d3ef21531c2ec12295b80025c
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-06-11 15:46:43 -0300

    CategoriesController unit tests is done

--- src/nestjs/src/categories/dto/update-category.dto.ts
+++ src/nestjs/src/categories/dto/update-category.dto.ts
@@ -1,7 +1,8 @@
 import { UpdateCategoryUseCase } from '@fc/micro-videos/category/application';
 
-export class UpdateCategoryDto implements UpdateCategoryUseCase.Input {
-  id: string;
+export class UpdateCategoryDto
+  implements Omit<UpdateCategoryUseCase.Input, 'id'>
+{
   name: string;
   description?: string;
   is_active?: boolean;
