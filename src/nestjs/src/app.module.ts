ba41b75c3ad6d8b8398e5fb92dd3118a01b4af03
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-08-16 09:48:17 -0300

    Ambiente configurado

--- src/nestjs/src/app.module.ts
+++ src/nestjs/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [CategoriesModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
